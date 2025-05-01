1dab55b507ac6fbf3608ebcd795fd4240b16af18
André Mathlener <andre.mathlener@ns.nl>
2025-05-01 17:18:35 +0200

    refactor: replace x and y in constructor of RoverState by Position

--- src/rover-state.ts
+++ src/rover-state.ts
@@ -5,22 +5,24 @@ export class RoverState {
     private readonly position: Position;
     private readonly direction: Direction;
 
-    private constructor(x = 0, y = 0, direction: Direction) {
-        this.position = new Position(x, y);
+    private constructor(position: Position, direction: Direction) {
+        this.position = position;
         this.direction = direction;
     }
 
     public static fromString(state: string): RoverState {
         const stateParameters = state.split(' ');
         if (stateParameters.length < 3) {
-            return new RoverState(0, 0, new Direction());
+            return new RoverState(new Position(0, 0), new Direction());
         }
 
         const x = parseInt(stateParameters[0], 10);
         const y = parseInt(stateParameters[1], 10);
+        const position = new Position(x, y);
+
         const direction = new Direction(stateParameters[2]);
 
-        return new RoverState(x, y, direction);
+        return new RoverState(position, direction);
     }
 
     public turnLeft() {
