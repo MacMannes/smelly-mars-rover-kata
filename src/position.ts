2d749f4fd0077dce5ca9e70317bd6de84e505284
André Mathlener <andre.mathlener@ns.nl>
2025-05-01 16:06:09 +0200

    feat: introduce Position class

---
+++ src/position.ts
@@ -0,0 +1,13 @@
+export class Position {
+    private x: number;
+    private y: number;
+
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public toString(): string {
+        return `${this.x} ${this.y}`;
+    }
+}
